8e8e660fc630103b6d746e2ca00a678a79010b39
Dehemi Weerakkody <dehemiweerakkodi2@gmail.com>
2023-12-19 23:51:24 +0530

    Added trying and context api

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"encoding/binary"
 	"flag"
 	"fmt"
@@ -8,6 +9,7 @@ import (
 	"log"
 	"net"
 	"os"
+	"time"
 )
 
 const UDP_BUFFER_SIZE = 8192
@@ -27,14 +29,6 @@ func main() {
 		os.Exit(1)
 	}
 
-	// Dial to the address with UDP
-	uConn, err := net.ListenUDP("udp", udpAddr)
-
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-
 	// Initialize TCP client
 
 	tcpAddr, err := net.ResolveTCPAddr("tcp4", *tcpAddrStr)
@@ -44,44 +38,83 @@ func main() {
 		os.Exit(1)
 	}
 
-	// Connect to the address with tcp
-	conn, err := net.DialTCP("tcp", nil, tcpAddr)
+retry:
+	// Dial to the address with UDP
+	uConn, err := net.ListenUDP("udp", udpAddr)
 
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
 
+	if err := ServeUDPOverTCP(tcpAddr, uConn); err != nil {
+		uConn.Close()
+		time.Sleep(time.Second)
+		goto retry
+	}
+}
+
+func ServeUDPOverTCP(tcpAddr *net.TCPAddr, uConn *net.UDPConn) error {
+	// Connect to the address with tcp
+	conn, err := net.DialTCP("tcp", nil, tcpAddr)
+
+	if err != nil {
+		return err
+	}
+
+	defer conn.Close()
+
 	log.Println("Connected to Server")
 
 	var started bool
 
+	fails := 0
+
+	udpCopyContext, cancel := context.WithCancel(context.Background())
+
+	defer cancel()
+
 	for {
+	tryAgain:
+
+		if fails > 5 {
+			return fmt.Errorf("retries exceeded")
+		}
+
 		var udpFrame = make([]byte, UDP_BUFFER_SIZE)
 
 		n, ad, err := uConn.ReadFromUDP(udpFrame)
 
 		// log.Println("got udp ", n)
 		if err != nil {
-			log.Fatal(err)
+			fails++
+			log.Printf("error - %v, retry - %d", err, fails)
+			goto tryAgain
 		}
 
 		if err := sendBuffer(udpFrame[:n], conn); err != nil {
 			log.Println(err)
-			break
+			return err
 		}
 
 		// log.Println("wrote udp")
 		if !started {
 			started = true
 			log.Println("got 1st packet. copying server to udp")
-			go copyServerToUDP(conn, uConn, *ad)
+			go copyServerToUDP(udpCopyContext, conn, uConn, *ad)
 		}
 	}
 }
 
-func copyServerToUDP(conn *net.TCPConn, cConn *net.UDPConn, uAddr net.UDPAddr) {
+func copyServerToUDP(ctx context.Context, conn *net.TCPConn, cConn *net.UDPConn, uAddr net.UDPAddr) {
 	for {
+		select {
+		case <-ctx.Done():
+			log.Println("copyServerToUDP context canceled")
+			return
+		default:
+		}
+
 		buf, err := recvbuffer(conn)
 
 		if err != nil {
@@ -89,14 +122,11 @@ func copyServerToUDP(conn *net.TCPConn, cConn *net.UDPConn, uAddr net.UDPAddr) {
 			break
 		}
 
-		// log.Print("from server length", length)
 		// Write to udp
 		if _, err := cConn.WriteToUDP(buf, &uAddr); err != nil {
 			log.Println(err)
 			break
 		}
-
-		// log.Print("wrote to udp", length)
 	}
 }
 
